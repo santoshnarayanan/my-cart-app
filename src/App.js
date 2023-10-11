3edf3e5ac7e26872490b438c25160337df3662ca
Santosh Narayanan <santosh33@gmail.com>
2023-10-12 00:41:19 +0200

    using GlobalContext in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 import Navbar from './Navbar';
 import CartContainer from './CartContainer';
+import { useGlobalContext } from './context';
+
 function App() {
+  const {loading} = useGlobalContext();
+  if(loading){
+    return(
+      <div className="loading">
+        <h1>loading</h1>
+      </div>
+    );
+  }
   return (
     <div className="App">
      <Navbar />
